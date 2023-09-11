8dedb12cf9078a9673d0f477d53f634cf7c1afa6
João Victor Novais <joao.dipaay@gmail.com>
2023-09-11 15:19:08 -0300

    refactor: new past for db, promoPrice replaced by price and added oldPrice

--- src/components/ItemsResult.jsx
+++ src/components/ItemsResult.jsx
@@ -1,111 +1,7 @@
 /* eslint-disable react/prop-types */
 import { AiFillStar } from "react-icons/ai";
+import { products } from "../db/Products";
 const ItemsResult = ({ section, filter, filterButton, filterPrice }) => {
-  const products = [
-    {
-      id: 1,
-      name: "Café com leite",
-      description: "O bom e velho café com leite",
-      category: "Coffee",
-      image: "src/assets/products/coffee/1.svg",
-      price: 6.99,
-      isPromotion: true,
-      promoPrice: 4.99,
-      rating: 4.9,
-    },
-    {
-      id: 2,
-      name: "Café com leite de cacau e caramelo",
-      description: "Leite vaporizado com mocha e cobertura de caramelo ",
-      category: "Coffee",
-      image: "src/assets/products/coffee/2.svg",
-      price: 14.99,
-      isPromotion: true,
-      promoPrice: 10.43,
-      rating: 4.6,
-    },
-    {
-      id: 3,
-      name: "Nitro cold brew",
-      description:
-        "Café preparado com nitrogênio, sem açúcar e creme aveludado",
-      category: "Coffee",
-      image: "src/assets/products/coffee/3.svg",
-      price: 16.99,
-      isPromotion: true,
-      promoPrice: 12.89,
-      rating: 4.4,
-    },
-    {
-      id: 4,
-      name: "Café Mocha",
-      description: "Café expresso com cobertura mocha, leite e creme batido",
-      category: "Coffee",
-      image: "src/assets/products/coffee/4.svg",
-      price: 12.99,
-      isPromotion: true,
-      promoPrice: 11.6,
-      rating: 4.7,
-    },
-    {
-      id: 5,
-      name: "Caramel Latte",
-      description: "Café expresso, leite fresco e caramelo",
-      category: "Coffee",
-      image: "src/assets/products/coffee/5.svg",
-      price: 9.99,
-      isPromotion: false,
-      promoPrice: 7.89,
-      rating: 4.8,
-    },
-    {
-      id: 6,
-      name: "Coca-cola lata 350ml",
-      description:
-        "Com a tradição e o gosto incomparável, ela se torna a melhor opção de refresco em qualquer momento. ",
-      category: "Drinks",
-      image: "src/assets/products/drinks/1.png",
-      price: 4.99,
-      isPromotion: false,
-      promoPrice: 3.89,
-      rating: 4.8,
-    },
-    {
-      id: 7,
-      name: "Pepsi lata 350ml",
-      description:
-        "A Pepsi Lata 350ml é um refrigerante de cola com aroma natural, apreciada pelo sabor suave e refrescante. ",
-      category: "Drinks",
-      image: "src/assets/products/drinks/2.png",
-      price: 4.89,
-      isPromotion: false,
-      promoPrice: 4.29,
-      rating: 4.7,
-    },
-    {
-      id: 9,
-      name: "NESCAU prontinho 200ml",
-      description: "Uma opção nutritiva e gostosa para seus filhos.",
-      category: "Drinks",
-      image: "src/assets/products/drinks/4.png",
-      price: 2.89,
-      isPromotion: true,
-      promoPrice: 1.96,
-      rating: 4.4,
-    },
-    {
-      id: 10,
-      name: "Croissant",
-      description: "O clássico pão francês.",
-      category: "Pastry",
-      image: "src/assets/products/foods/1.png",
-      price: 4.99,
-      isPromotion: false,
-      promoPrice: 4.29,
-      rating: 4.7,
-    },
-  ];
-
   const inputFilter = products.filter((product) => {
     const name = product.name
       .toLowerCase()
@@ -115,10 +11,15 @@ const ItemsResult = ({ section, filter, filterButton, filterPrice }) => {
     else return category;
   });
 
-  const priceResult = inputFilter.slice().sort((buttonA, buttonB) => {
-    if (filterPrice === "up") return buttonA.price - buttonB.price;
-    else if (filterPrice === "down") return buttonB.price - buttonA.price;
-    else return 0;
+  const priceResult = inputFilter.slice().sort((priceA, priceB) => {
+    if (filterPrice === "up") {
+      return priceA.price - priceB.price;
+    } else if (filterPrice === "down") {
+      return priceB.price - priceA.price;
+    } else {
+      // Se filterPrice for "position" ou outro valor não reconhecido, mantenha a ordem original
+      return 0;
+    }
   });
 
   const buttonResult = priceResult.filter((button) => {
@@ -153,7 +54,7 @@ const ItemsResult = ({ section, filter, filterButton, filterPrice }) => {
           </div>
           <div className="flex flex-col justify-center">
             <p className={product.isPromotion ? "inline" : "hidden"}>
-              R${product.promoPrice.toString().replace(".", ",")}
+              R${product.price.toString().replace(".", ",")}
             </p>
             <p
               className={
@@ -162,7 +63,7 @@ const ItemsResult = ({ section, filter, filterButton, filterPrice }) => {
                   : "items-center flex text-gray-500 line-through"
               }
             >
-              R${product.price.toString().replace(".", ",")}
+              R${product.oldPrice.toString().replace(".", ",")}
             </p>
           </div>
         </div>
