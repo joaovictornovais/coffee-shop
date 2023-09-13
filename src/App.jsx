0b2008fce2d2d22b6bd999f72c3e6f48a9c8f56c
João Victor Novais <joao.dipaay@gmail.com>
2023-09-13 11:05:52 -0300

    feat: searchbar dont belong Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
+import Searchbar from "./components/Searchbar";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
+  const [data, setData] = useState("");
+
+  const searchToFilters = (search) => {
+    setData(search);
+  };
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <div className="p-4 max-w-4xl mx-auto space-y-4">
+        <Searchbar searchToFilters={searchToFilters} />
+        <Routes>
+          <Route path="/" element={<Home data={data} />} />
+        </Routes>
+      </div>
     </Router>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,24 @@
-import Searchbar from "../components/Searchbar";
+/* eslint-disable react/prop-types */
 import Promotions from "../components/Promotions";
 import Filters from "../components/Filters";
 import { useState } from "react";
 
-const Home = () => {
-  const [data, setData] = useState("");
+const Home = ({ data }) => {
   const [query, setQuery] = useState("");
-
-  const searchToFilters = (search) => {
-    setData(search);
-  };
 
   const buttonFilter = (button) => {
     setQuery(button);
   };
 
   return (
-    <div className="p-4 max-w-4xl mx-auto space-y-4">
-      <Searchbar searchToFilters={searchToFilters} />
+    <>
       <Promotions searchToFilters={data} />
       <Filters
         searchToFilters={data}
         buttonFilter={buttonFilter}
         query={query}
       />
-    </div>
+    </>
   );
 };
 
