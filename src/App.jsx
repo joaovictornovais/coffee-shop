b29d5f22a8880673c9b77de4d106af6d0b4fdfc6
João Victor Novais <joao.dipaay@gmail.com>
2023-09-14 13:30:15 -0300

    refactor: removed searchbar from everything

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,11 @@ import ProductDetails from "./pages/ProductDetails";
 import Checkout from "./pages/Checkout";
 
 const App = () => {
-  const [data, setData] = useState("");
-
-  const searchToFilters = (search) => {
-    setData(search);
-  };
   return (
     <Router>
       <div className="p-4 max-w-4xl mx-auto space-y-4">
-        <Searchbar searchToFilters={searchToFilters} />
         <Routes>
-          <Route path="/" element={<Home data={data} />} />
+          <Route path="/" element={<Home />} />
           <Route path="/products/:id" element={<ProductDetails />} />
           <Route path="/checkout" element={<Checkout />} />
         </Routes>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,16 @@
 import Promotions from "../components/Promotions";
 import Filters from "../components/Filters";
 import { useState } from "react";
+import Searchbar from "../components/Searchbar";
 
-const Home = ({ data }) => {
+const Home = () => {
   const [query, setQuery] = useState("");
+
+  const [data, setData] = useState("");
+
+  const searchToFilters = (search) => {
+    setData(search);
+  };
 
   const buttonFilter = (button) => {
     setQuery(button);
@@ -12,6 +19,7 @@ const Home = ({ data }) => {
 
   return (
     <>
+      <Searchbar searchToFilters={searchToFilters} />
       <Promotions searchToFilters={data} />
       <Filters
         searchToFilters={data}
